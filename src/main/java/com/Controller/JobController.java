1c05cfa886010660a8d757db8f0085f84799aee9
李锴 <1121176110@qq.com>
2018-11-25 12:40:12 +0800

    [revise] 修改格式，将四个接口合并成一个接口

--- src/main/java/com/Controller/JobController.java
+++ src/main/java/com/Controller/JobController.java
@@ -6,9 +6,7 @@ import com.Param.Response.JobResponse;
 import com.Service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,47 +33,25 @@ public class JobController{
      * 最高优先权算法
      * @return
      */
-    @RequestMapping(value = "/Job/HPF",method = RequestMethod.GET)
-    public JobResponse HPF(){
+    @RequestMapping(value = "/Job/{method}",method = RequestMethod.POST)
+    public JobResponse Job(@PathVariable("method") String method){
         List<Job> p = new ArrayList<>();
         JobResponse jobResponses = new JobResponse();
         initTaskService.initTask(p,jobResponses);
-        return this.hpfService.HRRN(p,jobResponses);
-    }
-
-    /**
-     * 时间片轮转调度算法
-     * @return
-     */
-    @RequestMapping(value = "/Job/RR",method = RequestMethod.GET)
-    public JobResponse RR(){
-        List<Job> p = new ArrayList<>();
-        JobResponse jobResponses = new JobResponse();
-        initTaskService.initTask(p,jobResponses);
-        return this.rrService.CircleTime(p,jobResponses);
-    }
-
-    /**
-     * 先来先服务算法
-     * @return
-     */
-    @RequestMapping(value = "/Job/FCFS",method = RequestMethod.GET)
-    public JobResponse FCFS(){
-        List<Job> p = new ArrayList<>();
-        JobResponse jobResponses = new JobResponse();
-        this.fcfsService.initTask(p,jobResponses);
-        return this.fcfsService.FCFS(p,jobResponses);
-    }
-
-    /**
-     * 短作业优先算法
-     * @return
-     */
-    @RequestMapping(value = "/Job/SJF",method = RequestMethod.GET)
-    public JobResponse SJF(){
-        List<Job> p = new ArrayList<>();
-        JobResponse jobResponses = new JobResponse();
-        initTaskService.initTask(p,jobResponses);
-        return this.sjfService.SJF(p,jobResponses);
+        if(method.equals("HRRN")){
+            //最高优先权算法
+            return this.hpfService.HRRN(p,jobResponses);
+        }else if(method.equals("RR")){
+            //时间片轮转调度算法
+            return this.rrService.CircleTime(p,jobResponses);
+        }else if(method.equals("FCFS")){
+            //先来先服务算法
+            return this.fcfsService.FCFS(p,jobResponses);
+        }else if(method.equals("SJF")){
+            //短作业优先算法
+            return this.sjfService.SJF(p,jobResponses);
+        }else{
+            return null;
+        }
     }
 }
